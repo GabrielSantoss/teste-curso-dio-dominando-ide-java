60b870635252226f29ba82a990bb3d17d9e42bd0
GabrielSantoss <gabrazil03@hotmail.com>
2023-02-28 08:58:20 -0300

    first commit

---
+++ src/com/primeiro_projeto/PrimeiroProjeto.java
@@ -0,0 +1,15 @@
+package com.primeiro_projeto;
+
+import com.primeiro_projeto.model.Gato;
+
+public class PrimeiroProjeto {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato("Nelson", "rajado", 2);
+		
+		System.out.println(gato);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Gabriel
+ *
+ */
+module primeiro_projeto_java {
+}
